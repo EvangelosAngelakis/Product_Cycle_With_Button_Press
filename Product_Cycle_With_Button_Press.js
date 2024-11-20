cf69d1bc26cbf22fa9574df147c9946211dfa923
Evangelos Angelakis <Evangelosangel@gmail.com>
2024-11-20 15:02:38 +0000

    Added JS and start building code

---
+++ Product_Cycle_With_Button_Press.js
@@ -0,0 +1,10 @@
+const links = [
+    {text1:'',text2:'',href:'',src:''},
+    {text1:'',text2:'',href:'',src:''}
+];
+
+let linkIndex = 0;
+
+document.getElementById('dButton').addEventListener('click', function{}{
+    
+})
